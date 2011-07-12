484678977b1de4e21b1e1aad5d4e6d92f1f17d33
Maxim Verzakov <m2xver@gmail.com>
2011-07-12 18:56:54 +0700

    First steps in wx

---
+++ client/win1.py
@@ -0,0 +1,177 @@
+# gotoclass.py
+
+import wx
+
+class ConnectionDlgView(wx.Dialog):
+    
+    def __init__(self, parent, id, title):
+        wx.Dialog.__init__(self, parent, id, title, size=(250, 140))
+        panel = wx.Panel(self)
+
+        hbox = wx.BoxSizer(wx.VERTICAL)
+
+        fgs = wx.FlexGridSizer(2, 2, 9, 25)
+
+        thost = wx.StaticText(panel, label="Host")
+        tport = wx.StaticText(panel, label="Port")
+        
+        tc1 = wx.TextCtrl(panel)
+        tc2 = wx.TextCtrl(panel)
+        
+        fgs.AddMany([(thost), (tc1, 1, wx.EXPAND), (tport), 
+            (tc2, 1, wx.EXPAND)])
+
+        fgs.AddGrowableCol(1, 1)
+                
+        connectbtn = wx.Button(panel, wx.ID_ANY, label='Connect', size=(70, 27))
+
+        hbox.Add(fgs, proportion=1, flag=wx.ALL|wx.EXPAND, border=15)
+        hbox.Add(connectbtn, flag=wx.ALIGN_CENTER|wx.BOTTOM, border=15)
+        
+        panel.SetSizer(hbox)
+        
+class LoginDlgView(wx.Dialog):
+    
+    def __init__(self, parent, id, title):
+        wx.Dialog.__init__(self, parent, id, title, size=(300, 140))
+        panel = wx.Panel(self)
+
+        hbox = wx.BoxSizer(wx.VERTICAL)
+
+        fgs = wx.FlexGridSizer(2, 2, 3, 25)
+
+        tnick = wx.StaticText(panel, label="Nickname")
+        tpass = wx.StaticText(panel, label="Password")
+                
+        tc1 = wx.TextCtrl(panel)
+        tc2 = wx.TextCtrl(panel, style=wx.TE_PASSWORD)
+                
+        fgs.AddMany([(tnick), (tc1, 1, wx.EXPAND), (tpass), 
+            (tc2, 1, wx.EXPAND)])
+        
+        fgs.AddGrowableCol(1, 1)
+        
+        hbox.Add(fgs, proportion=1, flag=wx.ALL|wx.EXPAND, border=15)
+            
+        bttnbox = wx.BoxSizer(wx.HORIZONTAL)
+        
+        login_btn = wx.Button(panel, wx.NewId(), label='Login', size=(70, 27))
+        create_new_btn = wx.Button(panel, wx.NewId(), label='Create New', size=(100, 27))
+        
+        bttnbox.Add(login_btn, flag=wx.RIGHT, border=15)
+        bttnbox.Add(create_new_btn)
+
+        
+        hbox.Add(bttnbox, flag=wx.BOTTOM|wx.ALIGN_CENTER, border=15)
+        
+        panel.SetSizer(hbox)
+        
+        self.Bind(wx.EVT_BUTTON, self.OnNewUser, id=create_new_btn.GetId())
+        
+    def OnNewUser(self, event):
+        cDlg = CreateNewDlgView(None, -1, 'CreateNew')
+        cDlg.ShowModal()
+        cDlg.Destroy()
+        self.Close()
+        
+        
+class CreateNewDlgView(wx.Dialog):
+    
+    def __init__(self, parent, id, title):
+        wx.Dialog.__init__(self, parent, id, title, size=(300, 160))
+        panel = wx.Panel(self)
+
+        hbox = wx.BoxSizer(wx.VERTICAL)
+
+        fgs = wx.FlexGridSizer(3, 2, 3, 25)
+
+        tnick = wx.StaticText(panel, label="Nickname")
+        tpass1 = wx.StaticText(panel, label="Password")
+        tpass2 = wx.StaticText(panel, label="Password Again")
+                
+        tc1 = wx.TextCtrl(panel)
+        tc2 = wx.TextCtrl(panel, style=wx.TE_PASSWORD)
+        tc3 = wx.TextCtrl(panel, style=wx.TE_PASSWORD)
+                
+        fgs.AddMany([(tnick), (tc1, 1, wx.EXPAND), (tpass1), 
+            (tc2, 1, wx.EXPAND),(tpass2), (tc3, 1, wx.EXPAND)])
+        
+        fgs.AddGrowableCol(1, 1)
+        
+        hbox.Add(fgs, proportion=1, flag=wx.ALL|wx.EXPAND, border=15)
+            
+        bttnbox = wx.BoxSizer(wx.HORIZONTAL)
+        create_new_btn = wx.Button(panel, label='Create', size=(100, 27))
+        bttnbox.Add(create_new_btn)
+
+        
+        hbox.Add(bttnbox, flag=wx.BOTTOM|wx.ALIGN_CENTER, border=15)
+        
+        panel.SetSizer(hbox)
+
+class ChatView(wx.Frame):
+  
+    def __init__(self, parent, title):
+        super(ChatView, self).__init__(parent, title=title, 
+            size=(390, 350))
+        
+        self.ConnectionDlg()
+        self.LoginDlg() 
+        self.InitUI()
+        self.Centre()
+        self.Show()     
+        
+    def InitUI(self):
+    
+        panel = wx.Panel(self)
+
+        font = wx.SystemSettings_GetFont(wx.SYS_SYSTEM_FONT)
+        font.SetPointSize(9)
+        
+        chatbox = wx.BoxSizer(wx.VERTICAL)
+
+        chat_view_box = wx.BoxSizer(wx.HORIZONTAL)
+        viewctrl = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
+        chat_view_box.Add(viewctrl, proportion=1, flag=wx.EXPAND)
+        chatbox.Add(chat_view_box, proportion=1, flag=wx.EXPAND)
+
+        chatbox.Add((-1, 10))
+                        
+        chat_send_box = wx.BoxSizer(wx.HORIZONTAL)
+        tc = wx.TextCtrl(panel)
+        chat_send_box.Add(tc, proportion=1)
+        sendbtn = wx.Button(panel, label='Send', size=(70, 27))
+        chat_send_box.Add(sendbtn, flag=wx.LEFT, border=5)
+        chatbox.Add(chat_send_box, flag=wx.EXPAND)
+        
+        mainbox = wx.BoxSizer(wx.HORIZONTAL)
+        
+        mainbox.Add(chatbox, proportion=1, flag=wx.EXPAND|wx.ALL, border=10)
+        
+        mainbox.Add((-1, 10))
+        
+        listctr = wx.ListBox(panel, 26, (-1, -1), (170, 130), 
+		['xam_vz', 'g00se'], wx.LB_SINGLE) 
+        
+        mainbox.Add(listctr, proportion=1, flag=wx.RIGHT|wx.BOTTOM|wx.TOP, border=10)    
+
+        panel.SetSizer(mainbox)
+        
+    def ConnectionDlg(self):
+        cDlg = ConnectionDlgView(None, -1, 'Connect')
+        cDlg.ShowModal()
+        cDlg.Destroy()
+        
+    def LoginDlg(self):
+        cDlg = LoginDlgView(None, -1, 'Login')
+        cDlg.ShowModal()
+        cDlg.Destroy()
+        
+    
+
+
+if __name__ == '__main__':
+  
+    app = wx.App()
+    ChatView(None, title='Go To Class')
+    app.MainLoop()
