c01c63993f644cebd1976856a643b93a2a740aab
Maxim Verzakov <m2xver@gmail.com>
2011-07-20 14:34:01 +0700

    test stuff

--- client/chatprotocol.py
+++ client/chatprotocol.py
@@ -12,23 +12,20 @@ import sys
 
 class EchoClient(LineReceiver):
     end="Bye-bye!"
+    
     def connectionMade(self):
         self.sendLine("Hello, world!")
-        self.sendLine("What a fine day it is.")
-        self.sendLine(self.end)
+        self.sendLine("What a fine day it is.")        
 
     def lineReceived(self, line):
         print "receive:", line
-        if line==self.end:
-            self.transport.loseConnection()
-    
-    def sendMSG(self, msg):
-        self.sendLine(msg)
+        reactor.callLater(2, self.factory.msgtochat().callback, line)
+            
 
-class EchoClientFactory(pb.PBClientFactory):
+class EchoClientFactory(ClientFactory):
     
     protocol = EchoClient
-
+    
     def clientConnectionFailed(self, connector, reason):
         print 'connection failed:', reason.getErrorMessage()
         reactor.stop()
@@ -36,8 +33,20 @@ class EchoClientFactory(pb.PBClientFactory):
     def clientConnectionLost(self, connector, reason):
         print 'connection lost:', reason.getErrorMessage()
         reactor.stop()
+    
+    def msgtochat(self):
+        self.d = defer.Deferred()
+        return self.d
 
+def dosmth(msg):
+    print msg
 
+if __name__ == '__main__':
+    f = EchoClientFactory()
+    reactor.connectTCP('localhost', 1025, f)
+    d = f.msgtochat()
+    d.addCallback(dosmth)
+    reactor.run()
         
 
 
