50b24fc292ebe3f60086ac7d1ebd1c6713469f27
rogovvladimir <wwwwrogov@gmail.com>
2011-07-29 19:25:07 +0700

    docstings complete

--- server.py
+++ server.py
@@ -1,7 +1,16 @@
+"""
+Module contains the function which runs the server
+
+"""
 from twisted.internet import reactor
 from protocol.serverprotocol import ChatProtocolFactory
 
 def main():
+    """
+    This function starts the server through twisted framework
+    Than server will be available as localhost with 1025 port
+    
+    """
     factory = ChatProtocolFactory()
     reactor.listenTCP(1025, factory)
     reactor.run() 
